46e944508b192eb6e86fb784b7d93ae8acf1e8f0
saif <sirsaif99@gmail.com>
2023-02-23 16:25:24 +0200

    template root api

--- {{cookiecutter.app_slug}}/backend/src/app.py
+++ {{cookiecutter.app_slug}}/backend/src/app.py
@@ -4,3 +4,8 @@ from fastapi import APIRouter
 app = APIRouter(tags=["{{cookiecutter.app_tag}}"], prefix="/{{cookiecutter.app_prefix}}")
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World from {{cookiecutter.app_slug}}!"}
+
+
