18be1adf7652bc5ce134aea8d9dd53e60d8ffa5c
saif <sirsaif99@gmail.com>
2023-03-07 21:46:16 +0200

    root endpoint

--- {{cookiecutter.app_slug}}/backend/src/app.py
+++ {{cookiecutter.app_slug}}/backend/src/app.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 
 
 app = APIRouter(tags=["{{cookiecutter.app_tag}}"], prefix="/{{cookiecutter.app_prefix}}")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World from {{cookiecutter.app_slug}}!"}
-
+async def read_root():
+    return FileResponse("qna_app/frontend/build/index.html")
 
